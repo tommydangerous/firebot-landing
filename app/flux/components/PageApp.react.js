cfef8a5f9f3eb24667ca9924f0379e145fbf0ee3
Tommy Dang <quantumventuress@gmail.com>
2015-03-19 22:16:50 -0700

    added responsive text sizing and fixed images to make squares

--- app/flux/components/PageApp.react.js
+++ app/flux/components/PageApp.react.js
@@ -14,11 +14,11 @@ var PageApp = React.createClass({
       <div className="pages-index">
         <div className="gradient"></div>
         <div className="pattern"></div>
+        <div className="logo"></div>
         <div className="name">
           <h1>Firebot</h1>
           <p>Starts you up</p>
         </div>
-        <div className="logo"></div>
         <div className="coming">
           <p>{"Coming soon in 2015"}</p>
         </div>
