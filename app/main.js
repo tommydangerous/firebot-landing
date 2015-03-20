10d9cd779a91bb9a2ceda064feef56e2d4c56575
Tommy Dang <quantumventuress@gmail.com>
2015-03-20 07:32:27 -0700

    when window resizes, adjust opacity of logo

--- app/main.js
+++ app/main.js
@@ -2,7 +2,27 @@ var React = require('react');
 
 var PageApp = require('./flux/components/PageApp.react');
 
+var $ = require('jquery');
+
 React.render(
   <PageApp />,
   document.getElementById('page-app')
 );
+
+var logoOpacity = function(event) {
+  var height = $(window).height();
+  var logo   = $('.logo');
+  if (height < 380) {
+    logo.removeClass('opacity-1');
+    logo.addClass('opacity-2');
+  } else if (height < 480) {
+    logo.addClass('opacity-1');
+    logo.removeClass('opacity-2');
+  } else {
+    logo.removeClass('opacity-1');
+    logo.removeClass('opacity-2');
+  }
+};
+
+logoOpacity();
+window.onresize = logoOpacity;
