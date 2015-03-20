58332deef4a45162587e1c071ea2f52ce1db3063
Tommy Dang <quantumventuress@gmail.com>
2015-03-19 22:04:25 -0700

    finished

---
+++ app/flux/components/PageApp.react.js
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var PageApp = React.createClass({
+  getInitialState: function() {
+    return {
+      backgroundImageStyle: {
+        backgroundImage: 'url(img/logo.png)'
+      }
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="pages-index">
+        <div className="gradient"></div>
+        <div className="pattern"></div>
+        <div className="name">
+          <h1>Firebot</h1>
+          <p>Starts you up</p>
+        </div>
+        <div className="logo"></div>
+        <div className="coming">
+          <p>{"Coming soon in 2015"}</p>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = PageApp;
---
+++ app/main.js
@@ -0,0 +1,8 @@
+var React = require('react');
+
+var PageApp = require('./flux/components/PageApp.react');
+
+React.render(
+  <PageApp />,
+  document.getElementById('page-app')
+);
